e6f32e0ac2e4f90d0cc85d1c8bc4c0ba02446d47
SSaratKS <sarat@s3kdevelopers.com>
2022-11-14 21:29:57 +0000

    2 files modified

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,18 +5,38 @@ import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
 const Login = (props) => {
+  //Define state for validations
   const [enteredEmail, setEnteredEmail] = useState('');
   const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  // Re-evaluate form validation use useEffect
+  // Executes continously without [] for every state update or executes ONLY once when initiated
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
-    //add the functions itself as a dependency to check, if either of the functions have changed (OR) in the last cycle for useEffect to rerun
+    //console.log('useEffect Running');
+    return () => {
+      //console.log('useEffect Cleanup');
+    };
+  }, []);
+
+  // To execute useEffect when change occurs in functions in the dependency
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log('Checking form validty!');
+      // Execute only ONCE after keylogging is completed
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    // Execute a cleanup function
+    return () => {
+      console.log('CLEANUP');
+      // Clears previous timeout during its next render cycle
+      clearTimeout(identifier);
+    };
+    //add the function itself as a dependency to check, if either have changed (OR function) during its next render cycle
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import './index.css';
 
 import { AuthContextProvider } from './store/auth-context';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
