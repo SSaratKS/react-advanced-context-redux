b7927cd29ed10dfb7bd2b1fda94e637ee5f0a21d
SSaratKS <sarat@s3kdevelopers.com>
2022-11-15 14:09:27 +0000

    2 files modified, 2 added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Card from '../UI/Card/Card';
 import Button from '../UI/Button/Button';
 import classes from './Home.module.css';
 
+import UsersPage from '../../pages/UsersPage';
+
 const Home = (props) => {
   return (
-    <Card className={classes.home}>
-      <h1>Welcome back!</h1>
-      <Button onClick={props.onLogout}>Logout</Button>
-    </Card>
+    <Fragment>
+      <Card className={classes.home}>
+        <h1>Welcome back!</h1>
+        <Button onClick={props.onLogout}>Logout</Button>
+      </Card>
+      <UsersPage />
+    </Fragment>
   );
 };
 
